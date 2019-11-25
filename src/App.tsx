4fd528109643d9a61de546cecc1d6277ef441e71
Rafay <abdurrafay073@gmail.com>
2019-11-24 17:01:48 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,4 @@ const App: React.FC = () => {
   );
 };
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
 export default App;
